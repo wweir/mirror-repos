28c938660fbdecf229e64053e1e013b8cd7e277e
wweir <wweir@foxmail.com>
2023-12-12 21:52:51 +0800

    Update init-zpm-zsh.sh

--- init-zpm-zsh.sh
+++ init-zpm-zsh.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+set -ex
 
 mkdir -p zpm-zsh
 cd zpm-zsh
 
-git clone --mirror git@github.com:zpm-zsh/zpm.git \
-  && cd zpm.git \
-  && git remote add mirror git@e.coding.net:wweir/zpm-zsh/zpm.git \
-  && git remote show origin && git remote show mirror
+git clone --mirror git@github.com:zpm-zsh/zpm.git
+cd zpm.git
+git remote add mirror git@e.coding.net:wweir/zpm-zsh/zpm.git
