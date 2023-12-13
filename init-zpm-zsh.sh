181a328abf7e911448251ce3ce9963831858ea2e
wweir <wweir@foxmail.com>
2023-12-13 12:18:46 +0800

    Update init-zpm-zsh.sh

--- init-zpm-zsh.sh
+++ init-zpm-zsh.sh
@@ -11,3 +11,27 @@ for repo in zpm.git core-config.git helpers.git zsh-completions.git fast-syntax-
   git remote add mirror git@e.coding.net:wweir/$project/$repo
   git remote update origin && git push mirror
 done
+
+project=tj
+for repo in git-extras.git; do
+  git clone --mirror https://github.com/$project/$repo
+  cd $repo
+  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote update origin && git push mirror
+done
+
+project=agkozak
+for repo in zsh-z.git; do
+  git clone --mirror https://github.com/$project/$repo
+  cd $repo
+  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote update origin && git push mirror
+done
+
+project=miekg
+for repo in lean.git; do
+  git clone --mirror https://github.com/$project/$repo
+  cd $repo
+  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote update origin && git push mirror
+done
