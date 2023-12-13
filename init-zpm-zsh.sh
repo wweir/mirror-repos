a437c23323f7fcb33cdddd79b20c4743bf654878
wweir <wweir@foxmail.com>
2023-12-13 12:03:25 +0800

    Update init-zpm-zsh.sh

--- init-zpm-zsh.sh
+++ init-zpm-zsh.sh
@@ -4,7 +4,10 @@ set -ex
 mkdir -p zpm-zsh
 cd zpm-zsh
 
-git clone --mirror https://github.com/zpm-zsh/zpm.git
-cd zpm.git
-git remote add mirror git@e.coding.net:wweir/zpm-zsh/zpm.git
-git remote update origin && git push mirror
+project=zpm-zsh
+for repo in zpm.git core-config.git helpers.git zsh-completions.git fast-syntax-highlighting.git; do
+  git clone --mirror https://github.com/$project/$repo
+  cd $repo
+  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote update origin && git push mirror
+done
