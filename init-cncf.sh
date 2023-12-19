460998d25fd2e430ec6b5e18919852a6c67eac96
wweir <wweir@foxmail.com>
2023-12-19 11:24:40 +0800

    refactor: migrate to codeup

--- init-cncf.sh
+++ init-cncf.sh
@@ -4,27 +4,28 @@ set -ex
 mkdir -p cncf
 cd cncf
 WORK_DIR=$(pwd)
+ENDPOINT=https://codeup.aliyun.com/wweir
 
 project=kubernetes
 for repo in kubernetes.git; do
-  git clone --mirror git@github.com:$project/$repo
+  git clone --mirror https://github.com/$project/$repo
   cd $repo
-  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote add mirror $ENDPOINT/$project/$repo
   cd $WORK_DIR
 done
 
 project=opencontainers
 for repo in runc.git; do
-  git clone --mirror git@github.com:$project/$repo
+  git clone --mirror https://github.com/$project/$repo
   cd $repo
-  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote add mirror $ENDPOINT/$project/$repo
   cd $WORK_DIR
 done
 
 project=containerd
 for repo in containerd.git; do
-  git clone --mirror git@github.com:$project/$repo
+  git clone --mirror https://github.com/$project/$repo
   cd $repo
-  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote add mirror $ENDPOINT/$project/$repo
   cd $WORK_DIR
 done
--- init-zpm.sh
+++ init-zpm.sh
@@ -4,35 +4,36 @@ set -ex
 mkdir -p zpm
 cd zpm
 WORK_DIR=$(pwd)
+ENDPOINT=https://codeup.aliyun.com/wweir
 
 project=zpm-zsh
 for repo in zpm.git core-config.git helpers.git zsh-completions.git fast-syntax-highlighting.git; do
-  git clone --mirror git@github.com:$project/$repo
+  git clone --mirror https://github.com/$project/$repo
   cd $repo
-  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote add mirror $ENDPOINT/$project/$repo
   cd $WORK_DIR
 done
 
 project=tj
 for repo in git-extras.git; do
-  git clone --mirror git@github.com:$project/$repo
+  git clone --mirror https://github.com/$project/$repo
   cd $repo
-  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote add mirror $ENDPOINT/$project/$repo
   cd $WORK_DIR
 done
 
 project=agkozak
 for repo in zsh-z.git; do
-  git clone --mirror git@github.com:$project/$repo
+  git clone --mirror https://github.com/$project/$repo
   cd $repo
-  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote add mirror $ENDPOINT/$project/$repo
   cd $WORK_DIR
 done
 
 project=miekg
 for repo in lean.git; do
-  git clone --mirror git@github.com:$project/$repo
+  git clone --mirror https://github.com/$project/$repo
   cd $repo
-  git remote add mirror git@e.coding.net:wweir/$project/$repo
+  git remote add mirror $ENDPOINT/$project/$repo
   cd $WORK_DIR
 done
